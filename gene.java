973c2f4890f9bd0b14e4a720f7b2256d6eeeb324
SidIzG <33462891+SidIzG@users.noreply.github.com>
2018-02-07 22:47:20 -0500

    Create gene.java

---
+++ gene.java
@@ -0,0 +1,14 @@
+package project_2_1;
+
+public class gene {
+    String geneName;
+    double ig;
+    public gene (String geneName, double ig) {
+        this.geneName = geneName;
+        this.ig = ig;
+    }
+    
+    public double getIg() {
+        return ig;
+    }
+}
